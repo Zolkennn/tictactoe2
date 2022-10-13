ef2f3b6af7414d3f920fd806421a050f1e5f0269
Zolkennn <zolken13@gmail.com>
2022-10-13 22:18:17 +0200

    Version final dans ia ?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 
 def Demarée_Partie():
-    global Mario_rouge, Mario_Vert, main, score, zone_score, jeux
+    global Mario_rouge, Mario_Vert, Game_Over, main, score, zone_score, jeux, zone_joueur, joueur
     main = Tk()
     main.grid_rowconfigure(3, weight=1)
     main.grid_columnconfigure(3, weight=1)
@@ -33,11 +33,18 @@ def Demarée_Partie():
 
     main.config(menu=menubar)
 
-    zone_score = Label(main, text="Score: "+ str(score[0]) + "R "+ str(score[1]) + "V")
+    zone_score = Label(main, text="Score: " + str(score[0]) + "R " + str(score[1]) + "V")
     zone_score.pack(side=BOTTOM)
+    zone_joueur = Canvas(main, width=100, height=56, highlightthickness=0)
+    joueur = random.choice([Mario_rouge, Mario_Vert])
+    print(str(joueur))
+    zone_joueur.pack(side=BOTTOM)
+    zone_joueur.create_image(50, 30, image=joueur)
+
 
 def plateau():
-    global Clique, boutons, Etat, joueur
+    global Clique, boutons, Etat, c
+    c = 0
     boutons = [[0, 0, 0],
                [0, 0, 0],
                [0, 0, 0]]  # contiendra tout les 9 boutons du plateau de jeux
@@ -55,56 +62,111 @@ def plateau():
                                    highlightbackground="#1e1e1e")
             boutons[i][x].grid(column=i, row=x)
             boutons[i][x].bind("<Button-1>", lambda e=0, x=i, y=x: click(e, x, y))
-    joueur = random.choice(["Rouge", "Vert"])
-    print(joueur)
+    main.update()
+
+
 def redemare():
     main.destroy()
     Demarée_Partie()
 
+
 def relance():
-    global jeux
+    global jeux, joueur
     jeux.destroy()
     jeux = Frame(main)
     jeux.config(background="#2a2424")
     jeux.pack()
+    joueur = random.choice([Mario_rouge, Mario_Vert])
+    print(str(joueur))
+    zone_joueur.unbind("<Button-1>")
+    zone_joueur.config(width=100, height=56, highlightthickness=0)
+    zone_joueur.create_rectangle(0, 0, 100, 56, fill="lightgrey")
+    zone_joueur.pack(side=BOTTOM)
+    zone_joueur.create_image(50, 30, image=joueur)
     plateau()
+
+
 def finit(couleur):
+    global score
     # bloque tous les boutons
     for i in range(3):
         for x in range(3):
             Clique[i][x] = False
-    if couleur == "R":
-        score[0] += 1
+    zone_joueur.config(width=256, height=206)
+    zone_joueur.create_rectangle(0, 0, 256, 206, fill="lightgrey")
+    zone_joueur.create_image(128, 103, image=Game_Over)
+    zone_joueur.bind("<Button-1>", lambda x=1: relance())
+    if couleur == "red":
+        score[0] += 3
     else:
-        score[1] += 1
-    zone_score.config(text="Score: "+ str(score[0]) + "R "+ str(score[1]) + "V")
+        score[1] += 3
+    zone_score.config(text="Score: " + str(score[0]) + "R " + str(score[1]) + "V")
+
+
+def ligne(couleur, type, ou):
+    width = boutons[0][0].winfo_width()
+    height = boutons[0][0].winfo_height()
+    if type == "ligne":
+        for i in range(3):
+            boutons[i][ou].create_line(0, height/2, width, height/2, fill="dark"+couleur, width=10)
+    if type == "colone":
+        for i in range(3):
+            boutons[ou][i].create_line(width/2, 0, width/2, height, fill="dark"+couleur, width=10)
+    if type == "diagonalGD":
+        for i in range(3):
+            boutons[i][i].create_line(0, 0, width, height, fill="dark"+couleur, width=10)
+    if type == "diagonalDG":
+        for i in range(3):
+            boutons[2-i][i].create_line(width, 0, 0, height, fill="dark"+couleur, width=10)
+
+
+
+def egalite():
+    global score
+    zone_joueur.bind("<Button-1>", lambda x=1: relance())
+    zone_joueur.config(width=256, height=206)
+    zone_joueur.create_rectangle(0, 0, 256, 206, fill="lightgrey")
+    score[0] += 1
+    score[1] += 1
+    zone_score.config(text="Score: " + str(score[0]) + "R " + str(score[1]) + "V")
 
 
 def verif(a, b):
+    global c
+    c += 1
     x = Etat[b][a]
     if Etat[b] == [str(x), str(x), str(x)]:  # verif ligne
         finit(x)
+        ligne(x, "ligne", b)
     elif x == Etat[0][a] and x == Etat[1][a] and x == Etat[2][a]:  # verif colonne
         finit(x)
+        ligne(x, "colone", a)
     elif x == Etat[0][0] and x == Etat[1][1] and x == Etat[2][2]:  # verif diagonal gauche droite
         finit(x)
+        ligne(x, "diagonalGD", 0)
     elif x == Etat[0][2] and x == Etat[1][1] and x == Etat[2][0]:  # verif diagonal droite gauche
         finit(x)
+        ligne(x, "diagonalDG", 0)
+    elif c == 9:
+        egalite()
 
 
 def click(event, a, b):
     global joueur
+    print(c)
     if Clique[a][b]:
         Clique[a][b] = False
-        if joueur == "Rouge":
+        if joueur == Mario_rouge:
             boutons[a][b].create_image(36, 30, image=Mario_rouge)
-            joueur = "Vert"
-            Etat[b][a] = "R"
+            zone_joueur.create_image(50, 30, image=Mario_Vert)
+            joueur = Mario_Vert
+            Etat[b][a] = "red"
             verif(a, b)
         else:
             boutons[a][b].create_image(36, 30, image=Mario_Vert)
-            joueur = "Rouge"
-            Etat[b][a] = "V"
+            zone_joueur.create_image(50, 30, image=Mario_rouge)
+            joueur = Mario_rouge
+            Etat[b][a] = "green"
             verif(a, b)
 
 
